27c8be9cec1e462ccf77d10c9626fce9f84eda27
kingaap177 <93843288+kingaap177@users.noreply.github.com>
2025-09-30 15:49:09 +0200

    add flip cooldown

--- Assets/scripts/CameraDirectionSelector.cs
+++ Assets/scripts/CameraDirectionSelector.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class CameraDirectionSelector : MonoBehaviour
@@ -8,6 +9,13 @@ public class CameraDirectionSelector : MonoBehaviour
     public Camera WestCamera;
     private int currentCamera = 0;
     private int rotationAngle = 0;
+
+    public int DelayBetweenFlips = 2000;
+    private int FlipTimer = 0;
+
+    private int rotationDegreePerFlip = 90;
+
+    private bool isRotating = false;
 
     void DisableAllCameras()
     {
@@ -40,55 +48,63 @@ public class CameraDirectionSelector : MonoBehaviour
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SetCameraDirection();
     }
 
-    void rotatePlayer()
+    void RotatePlayer()
     {
         if (rotationAngle >= 360)
         {
-            rotationAngle = rotationAngle - 360;
+            rotationAngle -= 360;
         }
         if (rotationAngle < 0)
         {
-            rotationAngle = rotationAngle + 360;
+            rotationAngle += 360;
         }
 
         transform.rotation = Quaternion.Euler(0, rotationAngle, 0);
     }
 
-
-    // Update is called once per frame
     void Update()
     {
+        if (isRotating) return;
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            currentCamera = currentCamera + 1;
-            if (currentCamera > 3)
-            {
-                currentCamera = 0;
-            }
-            SetCameraDirection();
-
-            rotationAngle = rotationAngle + 90;
-            rotatePlayer();
+            currentCamera = (currentCamera + 1) % 4;
+            _ = RotateCameraAsync(-rotationDegreePerFlip);
         }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            currentCamera = currentCamera - 1;
-            if (currentCamera < 0)
-            {
-                currentCamera = 3;
-            }
-            SetCameraDirection();
-
-            rotationAngle = rotationAngle - 90;
-            rotatePlayer();
-
+            currentCamera = (currentCamera - 1 + 4) % 4;
+            _ = RotateCameraAsync(rotationDegreePerFlip);
         }
+    }
+
+    async Task DelayBetweenswitchingCameras()
+    {
+        while (FlipTimer < DelayBetweenFlips)
+        {
+            FlipTimer += 100;
+            await Task.Delay(100);
+        }
+        FlipTimer = 0;
+    }
+
+    async Task RotateCameraAsync(int rotationDelta)
+    {
+        isRotating = true;
+
+        SetCameraDirection();
+
+        rotationAngle += rotationDelta;
+        RotatePlayer();
+
+        await DelayBetweenswitchingCameras();
+
+        isRotating = false;
     }
 }
